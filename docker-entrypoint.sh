85abbd3731bc416b74370511f13120f3a066665c
Daniel Federschmidt <dfederschmidt@splunk.com>
2021-04-29 12:15:29 +0200

    add option to create superuser during run command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,14 @@ set -e
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+if [ "$URLPROMPT_ADMIN_USER" ] && [ ! -f .superuser ];
+then
+    DJANGO_SUPERUSER_PASSWORD="$URLPROMPT_ADMIN_PASS"
+    python manage.py createsuperuser --noinput --username $URLPROMPT_ADMIN_USER --email $URLPROMPT_ADMIN_USER@urlprompt.changeme
+    touch .superuser
+fi
+
+
+
 exec gunicorn -w 4 --bind 0.0.0.0:5000 --forwarded-allow-ips='*' urlprompt.wsgi
